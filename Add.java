834df837bd801d2250ab47473a8bf5dca9b6fb8b
Ria-Singh <ria.singh2311@gmail.com>
2022-05-01 23:23:10 -0700

    Added tests

--- Add.java
+++ Add.java
@@ -1,3 +1,7 @@
 public class Add{
 
+    public static int addOne(int i){
+
+        return i+1;
+    }
 }
---
+++ AddTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class AddTest {
+    @Test
+    public void addition() {
+    assertEquals(2, Add.addOne(3));
+}
+}
